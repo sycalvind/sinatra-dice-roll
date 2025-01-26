9acbb0233093a3f25577c8e97d0f7322808214de
sycalvind <114632354+sycalvind@users.noreply.github.com>
2025-01-26 03:27:07 +0000

    finished!

--- dice.rb
+++ dice.rb
@@ -23,32 +23,50 @@ get("/giraffe") do
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
-	
+  @title = "2d6"
+  @rolls = []
+  2.times do
+    die = rand(1..6)
+    @rolls.push(die) 
+  end
+  #sum = first_die + second_die
+=begin
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-
+=end
   erb(:two_six)
 end
 
 get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
-
+  @title = "2d10"
+  @rolls = []
+  2.times do
+    die = rand(1..10)
+    @rolls.push(die)
+  end
+  #sum = first_die + second_die
+=begin
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-
+=end
   erb(:two_ten)
 end
 
 get("/dice/1/20") do
+  @title = "1d20"
+  @rolls = []
   die = rand(1..20)
-  @outcome = "You rolled a #{die}."
+  @rolls.push(die)
+  #@outcome = "You rolled a #{@die}."
   erb(:one_twenty)
 end
 
 get("/dice/5/4") do
+  @title = "5d4"
+  @rolls = []
+  5.times do
+    die = rand(1..4)
+    @rolls.push(die)
+  end
+=begin
   first_die = rand(1..4)
   second_die = rand(1..4)
   third_die = rand(1..4)
@@ -56,5 +74,17 @@ get("/dice/5/4") do
   fifth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die + fifth_die
   @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
+=end
   erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @title = "100d6"
+  @rolls = []
+  100.times do
+    die = rand(1..6)
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
